94bfdb86530c40db5e28787e689e7638c20e4e75
drewrwilson <drew.r.wilson@gmail.com>
2015-09-11 17:38:53 -0400

    style

--- assets/js/misc-internet2016.js
+++ assets/js/misc-internet2016.js
@@ -1,6 +1,6 @@
 
 $( document ).ready(function() {
-    $( "#sticker" ).on( "click", function() {
+    $( "#id_user_internet2016_sticker_yes" ).on( "click", function() {
       $('#form-more-info').fadeToggle();
     });
 });

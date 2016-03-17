691a4ae0f35d7e0cd911206378aa0b0a50b60f86
Bill Brown <bill@echo.co>
2016-03-17 15:29:00 -0500

    [js] comments

--- assets/js/misc-internet2016.js
+++ assets/js/misc-internet2016.js
@@ -6,18 +6,29 @@ $( document ).ready(function() {
       $('#form-more-info').fadeToggle();
     });
 
-    $('#voter-guide-table').stacktable();
-    $('#voter-guide-table').stickyTableHeaders();
+    /////////////////////////////////////-+++-
+    // Scripts for the table at /voter-guide
+    //
 
-    var waypoints = $('#voter-guide-table tbody').waypoint({
-      handler: function(direction) {
-        if (direction == "down") {
-            $('#voter-guide-table').addClass('js-smallTableHeader');
-        } else if (direction == "up") {
-            $('#voter-guide-table').removeClass('js-smallTableHeader');            
+      // this reorganizes and stacks the table on mobile
+      // requires stacktable plugin: https://github.com/johnpolacek/stacktable.js/
+      $('#voter-guide-table').stacktable();
+
+      // this makes the thead of the table sticky as your scroll down the page
+      // requires StickyTableHeaders plugin: https://github.com/jmosbech/StickyTableHeaders
+      $('#voter-guide-table').stickyTableHeaders();
+
+      // this applies a class to the thead in order to hide large elements (like the photots) when you scroll down the page
+      // requires waypoints to trigger the class, hide animation applied via CSS
+      // https://github.com/imakewebthings/waypoints
+      var waypoints = $('#voter-guide-table tbody').waypoint({
+        handler: function(direction) {
+          if (direction == "down") {
+              $('#voter-guide-table').addClass('js-smallTableHeader');
+          } else if (direction == "up") {
+              $('#voter-guide-table').removeClass('js-smallTableHeader');            
+          }
         }
-
-      }
-    });
+      });
 
 });
